013c111824a4a9da6e8adeb5e6b78ae0dd8142ec
Reina Ogo <62832934+ogorei@users.noreply.github.com>
2022-02-08 22:43:29 +0900

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={tw`text-2xl font-bold p-4 bg-red-300 flex-row`}>
+      <Text>Reina no App</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+

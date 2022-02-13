20ededc1ffc0457a480162e8c489ebdd2c8f2247
Reina Ogo <62832934+ogorei@users.noreply.github.com>
2022-02-13 13:44:36 +0900

    updated welcome

--- screens/FlexBox.tsx
+++ screens/FlexBox.tsx
@@ -9,6 +9,31 @@ type Props = NativeStackScreenProps<RootStackParamList, 'FlexBox'>;
 // functional componentの型を適応するためVFCを使ってtype Propsのジェネリックpropsを定義する。そしてnavigationの機能を使えるようになる。
 
 export const FlexBox:VFC<Props> = ({navigation}) => {
+
+//     const catImages = "https://thecatapi.com/v1/images?api_key=1fbce736-a167-4b37-9e2e-3ed144944392"
+    
+//     // Implementation code where T is the returned data shape
+//     function callcats<T>(url: string): Promise<T> {
+//     return fetch(catImages)
+//       .then(response => {
+//         if (!response.ok) {
+//           throw new Error(response.statusText)
+//         }
+//         return response.json<T>()
+//       })
+//   }
+  
+//   // Consumer
+//     callcats<{ title: string; message: string }>('v1/posts/1')
+//         .then(({ title, message }) => {
+//         console.log(title, message)
+//         })
+//         .catch(error => {
+//         /* show error message */
+//         })
+
+    
+
     useEffect(()=> {
         console.log('reina');
         return ()=>{
@@ -17,23 +42,30 @@ export const FlexBox:VFC<Props> = ({navigation}) => {
 
     },[])
   return (
-    <View style={tw`items-center justify-center text-2xl font-bold p-4 bg-gray-300 flex-row`}>
-        <View style={tw`w-16 h-16 bg-indigo-500`}>
-            <Text>Image</Text>
+    <View style={tw`flex-1 items-center text-2xl font-bold p-4 bg-gray-300 flex-col justify-center`}>
+        <View style={tw`flex-row`}>
+            <View style={tw`w-16 h-16 bg-indigo-500 mx-4 justify-center items-center`}>
+                <Text>Image</Text>
+            </View>
+
+            <View style={tw`mx-4 justify-center items-center w-16 h-16 bg-pink-500`}>
+                <Text>Image</Text>
+            </View>
+
+            <View style={tw`w-16 h-16 bg-gray-500 justify-center items-center `}>
+                <Text>Image</Text>
+            </View>
         </View>
-        <View style={tw`justify-center items-center w-16 h-16 bg-pink-500`}>
-            <Text>Image</Text>
+
+        <View style={tw`my-8 bg-blue-400 `}>
+            <Button
+                title="Reduxへ行く"
+                onPress={()=> navigation.navigate('Redux')}
+                />
         </View>
-        <View style={tw`w-16 h-16 bg-gray-500`}>
-            <Text>Image</Text>
-        </View>
-      {/* <View style={tw`my-3 bg-blue-400`}>
-      <Button
-      title="Reduxへ行く"
-      onPress={()=> navigation.navigate('Redux')}
-      />
-      </View> */}
+
     </View>
+    
   );
 };
 
--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
 import React,{VFC,useEffect} from 'react';
 import tw from 'tailwind-react-native-classnames';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
@@ -8,6 +8,8 @@ import { Button } from 'react-native-elements';
 type Props = NativeStackScreenProps<RootStackParamList, 'FlexBox'>;
 
 export const Welcome:VFC<Props> = ({navigation}) => {
+    const image = { uri: 'https://images.unsplash.com/photo-1615789591457-74a63395c990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZG9tZXN0aWMlMjBjYXR8ZW58MHx8MHx8&w=1000&q=80' };
+
     useEffect(()=>{
         console.log('HELLO');
         return ()=>{
@@ -15,14 +17,17 @@ export const Welcome:VFC<Props> = ({navigation}) => {
         }
     },[])
   return (
-    <View style={tw`text-2xl font-bold p-4 bg-blue-200 flex-1 justify-center items-center`}>
-      <Text>ようこそREINAのアプリへ</Text>
-      <View style={tw`my-3 bg-pink-400`}>
-      <Button
-      title="flexBoxへ行く"
-      onPress={()=> navigation.navigate('FlexBox')}
-      />
-      </View>
+    <View style={tw`text-2xl font-bold bg-blue-200 flex-1 justify-center items-center`}>
+        <ImageBackground source={image} style={tw`flex-auto w-full justify-center items-center`}>
+            <Text style={tw`flex justify-center text-white items-center text-2xl`}>ようこそNEKOの世界へ</Text>
+            <View style={tw`mt-8`}>
+                <Button
+                title="ギャラリーへ行く"
+                onPress={()=> navigation.navigate('FlexBox')}
+                />
+            </View>
+        </ImageBackground>
+        
     </View>
   );
 };
